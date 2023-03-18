cd7946943c4eca33605b89e01d44683ed29f783d
arch user <None>
2023-03-18 11:03:21 -0600

    add new files for std

---
+++ lib/src/rustic_std.rs
@@ -0,0 +1,17 @@
+use crate::ast::Ident;
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
+lazy_static! {
+    pub(crate) static ref PRELUDE: HashMap<Ident, (Vec<Ident>, Ident)> = HashMap::from([
+        ("print_char".into(), (vec!["char".into()], "char".into())),
+        ("print".into(), (vec!["string".into()], "string".into()))
+    ]);
+}
+
+pub(crate) const PRELUDE_SRC: &[u8] = include_bytes!("std/prelude.c");
+
+extern "C" {
+    pub(crate) fn print(value: *const i8) -> *const i8;
+    pub(crate) fn print_char(value: i8) -> i8;
+}
